43d190114c5438465b1823b95ac0adb8e4ddafe2
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-06 18:58:37 +0200

    data preparation completed

--- LM/birnn.py
+++ LM/birnn.py
@@ -6,13 +6,20 @@ from torch.utils.data import Dataset, DataLoader
 from data_preparation import tokenizer, vocab
 
 class Model():
-    def __init__(self):
+    def __init__(self, seq_len, batch_size=16, shuffle=True, drop_last=True):
         tokens = tokenizer()
         vocab_obj = vocab()
         flattened = [tok for sent in tokens for tok in sent]
         tok_id = [vocab_obj[tok] for tok in flattened]
         
-class BiRNN(Dataset):
+        self.seq_len = seq_len
+        dataset = BiRNNDataSet(tok_id, self.seq_len)
+        loader = DataLoader(dataset=dataset, batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last)
+        
+        
+class BiRNNDataSet(Dataset):
     def __init__(self, tokens, seq_len):
         super().__init__()
         self.dataset = tokens
@@ -25,3 +32,4 @@ class BiRNN(Dataset):
         x = torch.tensor(self.dataset[index : index + self.seq_len], dtype=torch.long) # (T, )
         y = torch.tensor(self.dataset[index + 1 : index + self.seq_len + 1], dtype=torch.long) #(T, )
         return x, y
+    
