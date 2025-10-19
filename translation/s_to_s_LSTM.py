265ea1e6bd037657de614a82c10b771305cd0291
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-19 10:07:10 +0200

    Decoder-Encoder translation model initialized

--- translation/s_to_s_LSTM.py
+++ translation/s_to_s_LSTM.py
@@ -3,10 +3,13 @@ from torch import nn
 import torch.nn.init as init
 from tqdm.auto import tqdm
 from torch.utils.data import Dataset, DataLoader
+import pandas as pd
 
 class Model(nn.Module):
     def __init__(self):
         super().__init__()
+        self.raw_train = pd.read_csv("translation/data/wmt14_translate_fr-en_train.csv", delimiter=',', quotechar='"')
+        self.raw_valid = pd.read_csv("translation/data/wmt14_translate_fr-en_validation.csv", delimiter=',', quotechar='"')
         
 class TranslationDataset(Dataset):
     def __init__(self):
@@ -16,3 +19,6 @@ class STSLstm(nn.Module):
     def __init__(self):
         super().__init__()
         self.model = nn.LSTM()
+        
+
+model = Model()
