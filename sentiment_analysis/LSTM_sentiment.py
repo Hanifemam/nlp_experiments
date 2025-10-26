1fa6b553c77b74fbd5667fb92ade8b749b89763c
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-10-26 14:07:50 +0100

    Model initialized

--- sentiment_analysis/LSTM_sentiment.py
+++ sentiment_analysis/LSTM_sentiment.py
@@ -7,6 +7,7 @@ import pandas as pd
 from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torch.nn.utils.clip_grad import clip_grad_norm_
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
 
 class SentimentDataset(Dataset):
     def __init__(self, data):
@@ -25,11 +26,25 @@ class SentimentDataset(Dataset):
         
     
 class SentLSTM(nn.Module):
-    def __init__(self):
+    def __init__(self, vocab_size, embedding_dim=256, hidden_size=128, num_classes=3, pad_idx=0):
         super().__init__()
+        self.embed = nn.Embedding(embedding_dim=embedding_dim, num_embeddings=vocab_size,
+                                  padding_idx=pad_idx)
+        self.lstm = nn.LSTM(input_size=embedding_dim,
+                            hidden_size=hidden_size,
+                            batch_first=True)
+        self.linear = nn.Linear(hidden_size, 2)
         
+    def forward(self, x):
+        x = self.embed(x)
+        self.out, (h, c) = self.lstm(x)
+        h_last = h[-1]
+        logits = self.linear(h_last)
+        return logits
+        
+        self.lstm = nn.LSTM()
 class Model(nn.Module):
-    def __init__(self):
+    def __init__(self, batch_size, shuffle=True, drop_last=True, emb_size = 128, epochs = 10, lr=1e-3):
         super().__init__()
         device = "cuda" if torch.cuda.is_available() else "cpu"
         raw_train = pd.read_csv("/sentiment_analysis/data/sentiment_lstm_medium.csv")
@@ -42,17 +57,51 @@ class Model(nn.Module):
         
         special_tok = ["UNK"]
         
-        flattened_tok_train = [tok for sent, _ in tokenized_train for tok in sent]
+        PAD, UNK = "<pad>", "<unk>"
         vocab = build_vocab_from_iterator(
-            flattened_tok_train,
-            specials=special_tok,
+            (tokens for tokens, _ in tokenized_train),
+            specials=[PAD, UNK],
             special_first=True,
             min_freq=1
         )
+        vocab.set_default_index(vocab[UNK])
+        pad_idx = vocab[PAD]
         
-        vocab.set_default_index(vocab["UNK"])
-        train_ids = [(vocab(tok), class_) for tok, class_ in tokenized_train]
-        val_ids = [(vocab(tok), class_) for tok, class_ in tokenized_val]
+        def to_ids(tokens): 
+            return vocab.lookup_indices(tokens)
+
+        train_ids = [(to_ids(tokens), y) for tokens, y in tokenized_train]
+        val_ids   = [(to_ids(tokens), y) for tokens, y in tokenized_val]
+        def collate_batch(batch):
+            xs, ys = zip(*batch)
+            lengths = torch.tensor([len(x) for x in xs], dtype=torch.long)
+            xs_padded = pad_sequence(xs, batch_first=True, padding_value=pad_idx)
+            ys = torch.stack(ys)
+            return xs_padded, ys, lengths
+        
+        self.loader = DataLoader(
+            dataset=SentimentDataset(train_ids),
+            batch_size=batch_size,
+            shuffle=shuffle,
+            drop_last=drop_last,
+            collate_fn=collate_batch
+        )
+        self.val_loader = DataLoader(
+            dataset=SentimentDataset(val_ids),
+            batch_size=batch_size,
+            shuffle=False,
+            drop_last=False,
+            collate_fn=collate_batch
+        )
+        
+        vocab_size = len(vocab)
+        self.model = SentLSTM(vocab_size=vocab_size, embedding_dim=emb_size, hidden_size=128, num_classes=3, pad_idx=0)
+        
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr)
+        
+        self.epochs = epochs
+        self.batch_size = batch_size
         
     def label_conv_dict(self):
         return {"negative": 0,
