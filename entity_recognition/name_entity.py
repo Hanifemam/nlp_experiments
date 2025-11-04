98c2f50562ec1ca430742544a0470217c0f5a715
Hanif.Emamgholizadeh <h.emamgholizadeh@gmail.com>
2025-11-04 08:47:28 +0100

    Model added

--- entity_recognition/name_entity.py
+++ entity_recognition/name_entity.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn
 from torch.utils.data import Dataset, DataLoader
 from torchtext.vocab import build_vocab_from_iterator
+from tqdm.auto import tqdm
 
 def data_prep(dir):
     data_set = []
@@ -24,7 +25,7 @@ def data_prep(dir):
                     
                     
 class Model(nn.Module):
-    def __init__(self, batch_size=32, shuffle=True, drop_last=True):
+    def __init__(self, batch_size=32, shuffle=True, drop_last=True, epochs = 10, lr=1e-3):
         super().__init__()
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         train_dir = "entity_recognition/dataset/ner_train.conll"
@@ -43,7 +44,6 @@ class Model(nn.Module):
             min_freq=1
         )
         vocab_eng.set_default_index(vocab_eng[UNK])
-        self.vocab_eng = vocab_eng
         PAD_TAG = "<pad_tag>"
         vocab_tags = build_vocab_from_iterator(
             (tags for tags in target_train + target_val),
@@ -52,22 +52,22 @@ class Model(nn.Module):
             min_freq=1
         )
         vocab_tags.set_default_index(vocab_tags[PAD_TAG])
-        self.vocab_tags = vocab_tags  # Changed: store tag vocab for later decoding
-        self.pad_word_idx = vocab_eng[PAD]  # Changed: keep pad indices for batching
+        self.vocab_tags = vocab_tags 
+        self.pad_word_idx = vocab_eng[PAD] 
         self.pad_tag_idx = vocab_tags[PAD_TAG]
+        self.vocab_eng = vocab_eng
         train_ids = [vocab_eng.lookup_indices(x) for x in tokenized_train]
-        train_tag_ids = [vocab_tags.lookup_indices(tags) for tags in target_train]  # Changed: numerical tags
+        train_tag_ids = [vocab_tags.lookup_indices(tags) for tags in target_train] 
         val_ids = [vocab_eng.lookup_indices(x) for x in tokenized_val]
         val_tag_ids = [vocab_tags.lookup_indices(tags) for tags in target_val]
-        category_size = len(vocab_tags)
-        train_dataset = EntityDataset(list(zip(train_ids, train_tag_ids)))  # Changed: dataset now list of tuples
+        train_dataset = EntityDataset(list(zip(train_ids, train_tag_ids))) 
         val_dataset   = EntityDataset(list(zip(val_ids, val_tag_ids)))
         self.loader = DataLoader(
             dataset=train_dataset,
             batch_size=batch_size,
             shuffle=shuffle,
             drop_last=drop_last,
-            collate_fn=self.collate_fn  # Changed: pad batches via custom collate
+            collate_fn=self.collate_fn
         )
         self.val_loader = DataLoader(
             dataset=val_dataset,
@@ -76,9 +76,16 @@ class Model(nn.Module):
             drop_last=False,
             collate_fn=self.collate_fn
         )
+        self.model = EntityLSTM(
+            len(vocab_eng), len(vocab_tags),
+        ).to(self.device)
+
+        self.criterion = nn.CrossEntropyLoss(ignore_index=self.pad_tag_idx)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=lr) 
+        self.epochs = epochs
+        self.batch_size = batch_size
     
     def collate_fn(self, batch):
-        # Changed: pad sequences so batches are rectangular tensors
         tokens, tags = zip(*batch)
         max_len = max(len(seq) for seq in tokens)
         batch_size = len(tokens)
@@ -89,6 +96,31 @@ class Model(nn.Module):
             token_tensor[i, :length] = torch.tensor(token_seq, dtype=torch.long)
             tag_tensor[i, :length] = torch.tensor(tag_seq, dtype=torch.long)
         return token_tensor, tag_tensor
+    
+    def train_one_epoch(self, epoch: int):
+        self.model.train()
+        total_loss = 0.0
+        for src, tgt in tqdm(self.loader, desc=f"Epoch {epoch}/{self.epochs}",
+                            unit="batch", dynamic_ncols=True, leave=False):
+            src = src.to(self.device)
+            tgt = tgt.to(self.device)
+            
+            logits = self.model(src)
+            B, T, C = logits.shape  
+            loss = self.criterion(
+                logits.view(B * T, C),  
+                tgt.view(B * T)         
+            )
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            
+            total_loss += loss.item()
+            
+            tqdm.write(f"batch_loss={loss.item():.4f}") if False else None 
+            
+        avg_loss = total_loss / len(self.loader)
+        return avg_loss
 
 class EntityDataset(Dataset):
     def __init__(self, dataset):
@@ -96,10 +128,10 @@ class EntityDataset(Dataset):
         self.dataset = dataset
     
     def __len__(self):
-        return len(self.dataset)  # Changed: dataset now simple list
+        return len(self.dataset)  
     
     def __getitem__(self, idx):
-        return self.dataset[idx]  # Changed: collate_fn handles tensor conversion
+        return self.dataset[idx]
     
 class EntityLSTM(nn.Module):
     def __init__(self, src_vocab_size, category_size, emb_size=128, hidden_size=256, num_layers=1):
@@ -108,6 +140,7 @@ class EntityLSTM(nn.Module):
         self.encoder = nn.LSTM(input_size=emb_size, hidden_size=hidden_size,
                                num_layers=num_layers, batch_first=True)
         self.proj = nn.Linear(hidden_size, category_size)
+        
     def forward(self, src):
         enc_out, (h, c) = self.encoder(self.src_emb(src))
         logits = self.proj(enc_out)
